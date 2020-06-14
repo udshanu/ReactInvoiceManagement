f18863d03fc09ca9e76e6061827d9757e8c372fd
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-14 12:16:14 +0530

    Moved Data and Models Folders to the Infrastructure Project. Because Infrastructure project responsible for Data and database related developments

--- InvoiceManagementApp/InvoiceManagementApp.Infrastructure/Data/ApplicationDbContext.cs
+++ InvoiceManagementApp/InvoiceManagementApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using InvoiceManagementApp.Api.Models;
+﻿using InvoiceManagementApp.Infrastructure.Models;
 using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace InvoiceManagementApp.Api.Data
+namespace InvoiceManagementApp.Infrastructure.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
